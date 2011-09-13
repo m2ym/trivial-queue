494f168940cc28f293d93d138d019bd0e355571c
m2ym <tomo@cx4a.org>
2011-09-13 23:02:07 +0900

    Added QUEUE-TO-LIST.

--- queue.lisp
+++ queue.lisp
@@ -8,27 +8,31 @@
            #:enqueue
            #:dequeue
            #:queue-peek
-           #:queue-clear))
+           #:queue-clear
+           #:queue-to-list))
 (in-package :trivial-queue)
 
 (declaim (inline make-queue queue-size queue-empty-p
-                 enqueue dequeue queue-peek queue-clear))
+                 enqueue dequeue queue-peek queue-clear
+                 queue-to-list))
 
 (locally (declare (optimize (speed 3) (space 0)))
   (defstruct (queue (:conc-name)
                     (:constructor %make-queue))
     (%head () :type cons)
-    (%tail () :type cons)
+    (%last () :type cons)
     (%size 0  :type fixnum)))
 
 (defun make-queue (&key initial-contents)
   (declare (optimize (speed 3) (space 0)))
-  (let* ((cell (cons nil nil))
-         (queue (%make-queue :%head cell :%tail cell)))
-    (when initial-contents
-      (dolist (item initial-contents)
-        (enqueue queue item)))
-    queue))
+  (let ((cell (cons nil nil)))
+    (if initial-contents
+        (let ((head initial-contents)
+              (last (last initial-contents))
+              (size (length initial-contents)))
+          (setq last (setf (cdr last) (cons nil nil)))
+          (%make-queue :%head head :%last last :%size size))
+        (%make-queue :%head cell :%last cell))))
 
 (defun queue-size (queue)
   (declare (optimize (speed 3) (space 0)))
@@ -36,15 +40,15 @@
 
 (defun queue-empty-p (queue)
   (declare (optimize (speed 3) (space 0)))
-  (eq (%head queue) (%tail queue)))
+  (eq (%head queue) (%last queue)))
 
 (defun enqueue (queue item)
   (declare (optimize (speed 3) (space 0)))
-  (let ((tail (%tail queue)))
+  (let ((last (%last queue)))
     (locally (declare (optimize (safety 0)))
       (incf (%size queue)))
-    (setf (cdr tail) (setf (%tail queue) (cons nil nil))
-          (car tail) item)))
+    (setf (cdr last) (setf (%last queue) (cons nil nil))
+          (car last) item)))
 
 (defun dequeue (queue)
   (declare (optimize (speed 3) (space 0)))
@@ -59,5 +63,11 @@
 
 (defun queue-clear (queue)
   (declare (optimize (speed 3) (space 0)))
-  (setf (%head queue) (%tail queue))
+  (setf (%head queue) (%last queue))
   (values))
+
+(defun queue-to-list (queue)
+  (declare (optimize (speed 3) (space 0)))
+  (loop for cell on (%head queue)
+        while (cdr cell)
+        collect (car cell)))
